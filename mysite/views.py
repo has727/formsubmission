c1a44bad40a111fd6b7388e67b4f1bff4b65456d
has727 <has727@outlook.com>
2017-08-29 12:41:32 +0800

    1. added url for index.html 2. added required info and created database in 'models.py' 3. added views functions to display information that submitted in form 4. added 'base.html' 5. added 'index.html'. 'index.html' included forms that collects user data

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from django.template.loader import get_template
 
 # Create your views here.
+
+
+def index(request):
+	template = get_template('index.html')
+	try:
+		urid = request.GET['user_id']
+		urpass = request.GET['user_pass']
+	except:
+		urid = None
+	
+	if urid != None and urpass == '12345':
+		verified = True
+	else:
+		verified = False
+		
+	years = range(1960, 2021)
+	urfcolor = request.GET.getlist('fcolor')
+	yearok = request.GET.getlist('byear')
+	html = template.render(locals())
+	return HttpResponse(html)
+
